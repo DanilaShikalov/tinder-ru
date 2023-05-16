ab8d028c433cbda07b8184a582dd1b74de692be3
Wofyor <dan.shikalov@gmail.com>
2023-05-16 10:10:01 +0300

    update tests

--- src/test/java/com/example/digitalproject/services/servicesImpl/DocumentServiceImplTest.java
+++ src/test/java/com/example/digitalproject/services/servicesImpl/DocumentServiceImplTest.java
@@ -19,23 +19,25 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class DocumentServiceImplTest {
     @Mock
-    DocumentRepository documentRepository;
+    DocumentRepository documentRepository; // обязательно, чтобы задать условия на этот объект
     @Mock
     DocumentMapper documentMapper;
     @InjectMocks
-    DocumentServiceImpl documentService;
+    DocumentServiceImpl documentService; // вносим все mock объекты, которые нужны, внутрь этого объекта
 
     @Test
     void getDocument() {
-        Document document = new Document(1L, "test", "test", null);
-        DocumentGetDTO documentGetDTO = new DocumentGetDTO(1L, "test", "test");
+        Document document = new Document(1L, "test", "test", null); // создаем то, что наш сервис вернет
+        DocumentGetDTO documentGetDTO = new DocumentGetDTO(1L, "test", "test"); // опционально
 
-        when(documentRepository.findById(1L)).thenReturn(Optional.of(document));
-        when(documentMapper.entityToGet(document)).thenReturn(documentGetDTO);
+        when(documentRepository.findById(1L)).thenReturn(Optional.of(document)); // задаем условие (когда documentRepository вызовет findById(1L), то вернется document
+        when(documentMapper.entityToGet(document)).thenReturn(documentGetDTO); // опционально, условия на маппер (парсинг)
 
-        assertEquals(documentGetDTO, documentService.getEntity(1L));
-        verify(documentRepository).findById(1L);
-        verify(documentMapper).entityToGet(document);
+        assertEquals(documentGetDTO, documentService.getEntity(1L)); // вызываем у сервиса получения данных.
+        // Внутри вызванного метода вызывается documentRepository.findById из БД, но, так как мы поставили условие раннее, то вызова из БД не будет, а будет возвращен объект document
+        // Далее сравниваем ожидаемые данные с полученными
+        verify(documentRepository).findById(1L); // опционально
+        verify(documentMapper).entityToGet(document); // опционально
     }
 
     @Test
